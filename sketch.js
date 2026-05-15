cbe47a5a7cd4df3dcb82cd4befab998a28574443
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-05-15 20:51:10 +0000

    made missile sprite and made it follow the missiletruck

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let missileTruck;
+let missile;
 let backgroundImg;
 let missileTruckOrientation = "RIGHT";
 
@@ -12,12 +13,17 @@ async function main() {
     // 3. Configure the physics world
     world.gravity.y = 0;
 
-    // 4. Create your sprites
+    // 4. Create sprites
     missileTruck = new Sprite();
     missileTruck.x = 100; // Set initial X
-    missileTruck.y = ((backgroundImg.height*0.8)/2); // Set initial Y 330 is the Ground level
     missileTruck.diameter = 50;
-    missileTruck.img = await loadImage('assets/missile-truck.png');
+    missileTruck.img = loadImage('assets/missile-truck.png');
+
+    missile = new Sprite();
+    missile.x = 100; // Set initial X
+    missile.y = 200; // 0.86 makes the missile truck at ground level
+    missile.diameter = 50;
+    missile.img = await loadImage('assets/long-range-missile.png');
     
     
 
@@ -25,9 +31,11 @@ async function main() {
 
     // 5. Define the update loop inside main so it has access to variables
     q5.update = function () { // runs 60 times per second
+        missileTruck.y = 240; // 0.86 makes the missile truck at ground level
         background(backgroundImg);
         updateMissileTruck();
-        console.log("Y height: ", backgroundImg.height*0.8);
+        updateMissilePosition();
+        console.log("Y height: ", (backgroundImg.height*0.86)/2);
     };
 }
 
@@ -59,6 +67,15 @@ function updateMissileTruck(){
     if (missileTruck.x>600){
         missileTruck.x = 600;
     }
+}
+
+function updateMissilePosition(){
+    if(missileTruckOrientation == "RIGHT"){
+        missile.x = missileTruck.x-100;
+    }else{
+        missile.x = missileTruck.x+80;
+    }
+
 }
 
 // Execute the game
