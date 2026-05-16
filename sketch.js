d292af3c6ae13525a6f188c9315a7ab63fafccee
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-05-16 04:44:43 +0000

    made the game more responsive by making the sizes and positions of the different sprite dependant on the width and height of the screen

--- sketch.js
+++ sketch.js
@@ -2,32 +2,50 @@ let missileTruck;
 let missile;
 let backgroundImg;
 let missileTruckOrientation = "RIGHT";
-let groundYLevel = (window.screen.height/2)*0.7; // 0.7 makes the missile truck at ground level -1=top of screen, 1=bottom of screen
-let leftTruckBound = (window.screen.width/2)*-0.5; //-1=left of screen, 1=right of screen
-let rightTruckBound = (window.screen.width/2)*0.9;
+let groundYLevel;
+let leftTruckBound;
+let rightTruckBound;
+let truckSize;
+let missileSize;
 
 async function main() {
     // 1. Initialize Canvas first
-    await Canvas();
+    await Canvas('1:1');
+    
     
     // 2. Load your image safely using await
     backgroundImg = await loadImage('assets/Background.png');
+    groundYLevel = (height/2)*0.78; // 0.78 makes the missile truck at ground level -1=top of screen, 1=bottom of screen
+    leftTruckBound = (width/2)*-0.5; //-1=left of screen, 1=right of screen
+    rightTruckBound = (width/2)*0.9;
+    truckSize = (width/2)*0.001;
+    missileSize = (width/2)*0.001;
+    
 
     // 3. Configure the physics world
-    world.gravity.y = 0;
+    
 
     // 4. Create sprites
     missileTruck = new Sprite();
     missileTruck.x = 100; // Set initial X
     missileTruck.y = groundYLevel; 
     missileTruck.diameter = 50;
+
+
+    
     missileTruck.img = loadImage('assets/missile-truck.png');
 
     missile = new Sprite();
     missile.x = 100; // Set initial X
-    missile.y = missileTruck.y-35; // 0.86 makes the missile truck at ground level
+    missile.y = missileTruck.y-(35*truckSize); // 0.86 makes the missile truck at ground level
+    missile.scale = missileSize;
     missile.diameter = 50;
     missile.img = await loadImage('assets/long-range-missile.png');
+
+
+
+    missile.overlaps(missileTruck); //This tells the physics engine to not create physical forces between the missile and missle truck that would push the two sprites apart.
+
     
     
 
@@ -36,11 +54,11 @@ async function main() {
     // 5. Define the update loop inside main so it has access to variables
     q5.update = function () { // runs 60 times per second
         
-        background(backgroundImg);
+        image(backgroundImg, 0, 0, width, height);
         updateMissileTruck();
         updateMissilePosition();
-        console.log("X height: ", window.screen.width);
-        console.log("Y height: ", window.screen.height);
+        console.log("X width: ", width);
+        console.log("Y height: ", height);
     };
 }
 
@@ -48,19 +66,19 @@ function updateMissileTruck(){
     if(kb.pressing('left')){
         console.log("LEFT");
         missileTruckOrientation = "LEFT";
-        missileTruck.vel.x -= 0.5; // Accelerate left
+        missileTruck.vel.x -= (0.00025*width); // Accelerate left
         
     }
     if(kb.pressing('right')){
         console.log("RIGHT");
         missileTruckOrientation = "RIGHT";
-        missileTruck.vel.x += 0.5; // Accelerate right
+        missileTruck.vel.x += (0.00025*width); // Accelerate right
     
     }
     if (missileTruckOrientation == "LEFT"){
-        missileTruck.scale.x = -1; // Flips the sprite asset left
+        missileTruck.scale.x = -truckSize; // Flips the sprite asset left
     }else{
-        missileTruck.scale.x = 1; // Flips the sprite asset right
+        missileTruck.scale.x = truckSize; // Flips the sprite asset right
     }
     // Add friction to stop
     missileTruck.vel.x *= 0.9;
@@ -72,13 +90,14 @@ function updateMissileTruck(){
     if (missileTruck.x>rightTruckBound){
         missileTruck.x = rightTruckBound;
     }
+    missileTruck.scale.y = truckSize;
 }
 
 function updateMissilePosition(){
     if(missileTruckOrientation == "RIGHT"){
-        missile.x = missileTruck.x-100;
+        missile.x = missileTruck.x-(100*truckSize);
     }else{
-        missile.x = missileTruck.x+80;
+        missile.x = missileTruck.x+(80*truckSize);
     }
 
 }
