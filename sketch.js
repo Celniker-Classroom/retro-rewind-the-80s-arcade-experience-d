e0187757900867a9b4fa845a5101c05f50b5a9f5
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-05-19 01:57:27 +0000

    made wave system, updateenemymissiles function which detects if missile collides with the city or the truck, and city health.

--- sketch.js
+++ sketch.js
@@ -6,11 +6,10 @@ let explosions = [];
 let supplies = [];
 
 let missilesRemaining = 5;
-let fuelRemaining = 100;
+let cityHealth = 100;
 
-let waveActive = false;
 let waveCooldown = 0;
-
+let waveState = "COOLDOWN"; // COOLDOWN, STARTING, ACTIVE.
 
 
 let missileTruck;
@@ -23,6 +22,8 @@ let rightTruckBound;
 let truckSize;
 let missileSize;
 let isMissileFired = false;
+let cityLeftBound;
+let cityRightBound;
 
 async function main() {
     // 1. Initialize Canvas first
@@ -32,8 +33,15 @@ async function main() {
     // 2. Load your image safely using await
     backgroundImg = await loadImage('assets/Background.png');
     groundYLevel = (height/2)*0.78; // 0.78 makes the missile truck at ground level -1=top of screen, 1=bottom of screen
-    leftTruckBound = (width/2)*-0.5; //-1=left of screen, 1=right of screen
+
+
+    leftTruckBound = (width/2)*0; //-1=left of screen, 1=right of screen
     rightTruckBound = (width/2)*0.9;
+    cityLeftBound = (width/2)*-0.9;
+    cityRightBound = (width/2)*-0.6;
+
+
+
     truckSize = (width/2)*0.001;
     missileSize = (width/2)*0.001;
     
@@ -45,7 +53,7 @@ async function main() {
     missileTruck = new Sprite();
     missileTruck.x = 100; // Set initial X
     missileTruck.y = groundYLevel; 
-    missileTruck.diameter = 50;
+    missileTruck.diameter = 300;
 
 
     
@@ -61,17 +69,30 @@ async function main() {
 
 
     missile.overlaps(missileTruck); //This tells the physics engine to not create physical forces between the missile and missle truck that would push the two sprites apart.
-    startWave();
     
     
+
+
+    
+
 
 
 
     // 5. Define the update loop inside main so it has access to variables
     q5.update = function () { // runs 60 times per second
+
+
+        if(waveState == "COOLDOWN"){
+            waveState = "STARTING";
+            setTimeout(() => {
+                startWave();
+            }, 5000);
+        }
+
         
         image(backgroundImg, 0, 0, width, height);
         updateMissileTruck();
+        updateEnemyMissiles();
 
         if(isMissileFired){
             fireMissile();
@@ -81,6 +102,10 @@ async function main() {
         
         if (kb.presses('space')) {
             isMissileFired = true;
+        }
+
+        if(cityHealth<=0){
+            gameOver(); //TODO
         }
     };
 }
@@ -92,12 +117,10 @@ async function main() {
 
 function updateMissileTruck(){
     if(kb.pressing('arrowLeft')){
-        console.log("LEFT");
         missileTruckOrientation = "LEFT";
         missileTruck.vel.x -= (0.00025*width); // Accelerate left        
     }
     if(kb.pressing('arrowRight')){
-        console.log("RIGHT");
         missileTruckOrientation = "RIGHT";
         missileTruck.vel.x += (0.00025*width); // Accelerate right    
     }
@@ -133,7 +156,6 @@ function updateMissilePositionAndRotation(){
     //update missile rotation
     missile.rotationSpeed = 0.0;
     if(kb.pressing('a')){
-        console.log("LEFT");
         // Set the center of mass to the bottom edge-center of the sprite so the rotation looks more natural
         missile.centerOfMass = { x: 0, y: missile.halfHeight };
 
@@ -169,7 +191,6 @@ function updateMissilePositionAndRotation(){
         missile.rotation = Math.abs(missile.rotation); //changes missile's rotation to match missile truck's orientation
     }
 
-    console.log(missile.rotation);
 
 }
 
@@ -188,33 +209,80 @@ function fireMissile(){
 }
 
 function spawnEnemyMissile(){
-    let missile = new Sprite();
-    missile.x = random(-width/2, width/2); // Set initial X
-    missile.y = random(-height/2, -height/4); 
-    let cityLeftBound = (width/2)*-0.9;
-    let cityRightBound = (width/2)*-0.6;
-    let targetX = random(cityLeftBound, cityRightBound);
+    let enemyMissile = new Sprite();
+    enemyMissile.x = random(-width/2, width/2); // Set initial X
+    enemyMissile.y = random(-height/2, -height/4); 
+
+    let missileTargetingTruckProbability = 25; //out of 100
+    let targetX;
+    if(random(1, 100)>missileTargetingTruckProbability){
+        targetX = random(cityLeftBound, cityRightBound);
+    }else{
+        targetX = missileTruck.x;
+    }
+    
     let targetY = groundYLevel;
-    missile.scale = missileSize;
-    missile.diameter = 50;
-    missile.img = loadImage('assets/long-range-missile.png');
-    missile.rotation = missile.angleTo(targetX, targetY)+90;
-    missile.direction = missile.angleTo(targetX, targetY);
-    missile.speed= random(2, 10);
-    missile.rotationLock = true; // Prevents rotation on collision with other enemy missiles
+    enemyMissile.scale = missileSize;
+    enemyMissile.diameter = 50;
+    enemyMissile.img = loadImage('assets/long-range-missile.png');
+    enemyMissile.rotation = enemyMissile.angleTo(targetX, targetY)+90;
+    enemyMissile.direction = enemyMissile.angleTo(targetX, targetY);
+    enemyMissile.speed= random(2, 3);
+    enemyMissile.rotationLock = true; // Prevents rotation on collision with other enemy missiles
+    enemyMissiles.push(enemyMissile);
 
 }
 
+function updateEnemyMissiles(){
+    for(let i = enemyMissiles.length - 1; i >= 0; i--){
+        let enemyMissile = enemyMissiles[i];
+        // missile hit ground
+        if(enemyMissile.colliding(missileTruck)){
+            explodeMissile(enemyMissile);
+            stunTruck(); //TODO
+        }
+        if(enemyMissile.y > (height/2)*0.7){ // (height/2)*0.7 is little above ground y level
+            explodeMissile(enemyMissile);
+            enemyMissiles.splice(i, 1);
+            if(enemyMissile.x>cityLeftBound && enemyMissile.x<cityRightBound){
+                cityHealth-=10;
+                console.log("City Health:"+ cityHealth);
+            }
+
+        }
+        if (missile.colliding(enemyMissile)){
+            explodeMissile(enemyMissile);
+            enemyMissiles.splice(i, 1);  
+        }
+    }
+    if (waveState == "ACTIVE" && enemyMissiles.length==0){
+        waveState = "COOLDOWN";
+    }
+
+}
+function explodeMissile(missile){
+        missile.delete();
+        
+}
+
 function startWave(){
-    waveActive = true;
     let missileCount = 3 + (2*wave);
-    let delay = 1000; //in milliseconds
+    let delay = Math.max(1025-(25*wave), 500); //in milliseconds and spawns faster each wave
     for(let i = 0; i<missileCount; i++){
         setTimeout(() => {
             spawnEnemyMissile();
         }, i * delay);
     }
+    waveState = "ACTIVE";
+    wave++;
 }
 
+function stunTruck(){
+    console.log("Your truck is stunned!");
+}
+
+function gameOver(){
+    console.log("Game over!");
+}
 // Execute the game
 main();
